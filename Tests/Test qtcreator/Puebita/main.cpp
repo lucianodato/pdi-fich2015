e8f93c34cbefe5df038b1577dbc290274ddeef61
Luciano <lucianodato@gmail.com>
2015-03-19 12:06:15 -0300

    Mas gitignore. Explorando como funciona cmake

--- Tests/Test qtcreator/Puebita/main.cpp
+++ Tests/Test qtcreator/Puebita/main.cpp
@@ -4,7 +4,7 @@ using namespace cimg_library;
 
 int main()
 {
-    CImg<unsigned char> image("../../../images/lena.jpg"), visu(500,400,1,3,0);
+    CImg<unsigned char> image("../../../../images/lena.jpg"), visu(500,400,1,3,0);
     const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
     image.blur(2.5);
     CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
