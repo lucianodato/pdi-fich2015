631b273e25903a28b98cb4f1ce7cda246cee9690
Luciano <lucianodato@gmail.com>
2015-03-19 11:46:09 -0300

    Cambiada la estructura de directorios

---
+++ Tests/test cris/wors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <CImg.h>
+int main()
+{
+std::cout << "run CImg hello world..." << std::endl;
+cimg_library::CImg<unsigned char> img;
+std::string strFileInput = "../../images/lena.jpg";
+std::string strFileOutput = "../../images/llena.png";
+img = cimg_library::CImg<unsigned char>(strFileInput.c_str());
+std::cout << "load " << strFileInput << "..." << std::endl;
+std::cout << "width = " << img.width() << std::endl;
+std::cout << "height = " << img.height() << std::endl;
+std::cout << "color = " << img.spectrum() << std::endl;
+img.display("Input image");
+img.save_png(strFileOutput.c_str());
+return 0;
+}
