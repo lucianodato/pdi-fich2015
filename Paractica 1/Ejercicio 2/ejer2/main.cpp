c193d2d42f9cb1801a3c0c9381c9197ab4cb3d27
Luciano <lucianodato@gmail.com>
2015-03-19 19:05:31 -0300

    Lo de hoy

--- Paractica 1/Ejercicio 2/ejer2/main.cpp
+++ Paractica 1/Ejercicio 2/ejer2/main.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    const char* filename = cimg_option("-i","../../../../images/cameraman.tif","Image file\n");
+    //Imagen color, 8 bits de profundidad por componente.
+    //Asigna 0 a todos los pıxeles
+    //Visualiza
+    CImg<unsigned char> img1(filename);
+
+    //Carga de disco
+    CImgDisplay vent1(img1,"original");
+
+    //Encontrar los pixeles de a cara del fotografo (version de libreria)
+    CImg<unsigned char> img2 = img1.get_crop(94,38,130,80);
+    CImgDisplay vent2(img2,"recortada");
 }
 
