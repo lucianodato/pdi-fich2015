9c49f61c52fb0693a1b5b66ea1354a3ad999a0cd
cristian <cristiaan3003@gmail.com>
2015-03-20 13:21:22 -0300

    pude hacer commit- ejercicio 4 5 6

--- practica 1/Ejercicio 2/ejer2/main.cpp
+++ practica 1/Ejercicio 2/ejer2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include "CImg.h"
+using namespace cimg_library;
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -16,5 +17,7 @@ int main(int argc, char *argv[])
     //Encontrar los pixeles de a cara del fotografo (version de libreria)
     CImg<unsigned char> img2 = img1.get_crop(94,38,130,80);
     CImgDisplay vent2(img2,"recortada");
+
+    return 0;
 }
 
--- practica 1/Ejercicio 6/ejer6/main.cpp
+++ practica 1/Ejercicio 6/ejer6/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
-
+#include "CImg.h"
+using namespace cimg_library;
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+
+    CImg<unsigned char> img;
+    CImgList<unsigned char> listaImagenes;
+    CImgDisplay displayImagen;
+
+    img.load("../../../../images/huang2.jpg");
+    listaImagenes.insert(img);
+    for(unsigned i=8; i>1; i--){
+        listaImagenes.insert(img.get_quantize(i));
+    }
+    displayImagen.assign(listaImagenes, "Quantizacion 8bit(256 niveles) to 2 bit(2 niveles )");
+
+    while(!displayImagen.is_closed()){
+        displayImagen.wait_all(); // esperamos algun evento en cualquier display
+    }
+
     return 0;
 }
 
