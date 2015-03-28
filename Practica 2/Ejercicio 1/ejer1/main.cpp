a6807a7cd2a20a0e514613c7b1ba0ed68ba729ef
cristian <cristiaan3003@gmail.com>
2015-03-28 12:01:08 -0300

    ejercicio 1,2,3 - lut anda, reducir ruido no?? revisar

--- Practica 2/Ejercicio 1/ejer1/main.cpp
+++ Practica 2/Ejercicio 1/ejer1/main.cpp
@@ -7,31 +7,23 @@ using namespace cimg_library;
 using namespace std;
 
 //Parametros de inicializacion
-int alpha=1, offset=0;
+int alpha=-1, offset=255;
 
 int main(int argc, char *argv[])
 {
 
-    const char* filename = cimg_option("-i","../../../../images/botellas.tif","Image file\n");
-    CImg<unsigned char> original(filename),modificada(original.width(),original.height(),1,1),imagen_lut,intensidad(256,256,1,1);
-
-
-
-
-    //genero la LUT y reemplazo en la imagen
-    imagen_lut = lut(alpha,offset);
-
-    cimg_forXY(modificada,i,j)
-    modificada(i,j) = imagen_lut(1,(int)*original.data(i,j,0,0));
-
+    const char* filename = cimg_option("-i","../../../../images/rmn.jpg","Image file\n");
+    CImg<unsigned char> original(filename),modificada(original.width(),original.height(),1,1);
+     CImg<unsigned char> lut(generar_lut_lin(alpha,offset));
+     modificada=transformacion(modificada,lut);
+     lut.display("Lut");
 
     //Display
-   CImgDisplay vent0(original,"original"),draw_curve(intensidad,"Curva de intensidad"), vent1(modificada,"modificada");
+   CImgDisplay vent0(original,"original"),draw_curve(lut,"Curva de intensidad"), vent1(modificada,"modificada");
 
    while(!vent0.is_closed() && !vent1.is_closed() && !draw_curve.is_closed()){
        //Esto es para que no consuma recursos cuando la ventana esta abierta
-       vent0.wait();
-       vent1.wait();
+       vent0.wait_all();
        draw_curve.wait();
    }
 
---
+++ Practica 2/Ejercicio 2/ejer2/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "CImg.h"
+#include <vector>
+#include "funciones.h"
+
+using namespace cimg_library;
+using namespace std;
+
+//Parametros de inicializacion
+float c=1;
+float cc=1,potencia=2.0;
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+    CImg<unsigned char> img1,img2;
+    const unsigned char white[] = { 255,255,255 };
+    CImgList<unsigned char> listaImagenes;
+    img1.load("../../../../images/rmn.jpg");//carga imagen en memo
+       //img1.load("../PDI/Imagenes/autos.png");//carga imagen en memo
+    CImg<unsigned char>  visu (img1.width(),img1.height(),1,1);
+    img2=img1;
+    CImg<unsigned char> lut(generar_lut_logb(c));
+    img2=transformacion(img2,lut);
+    listaImagenes.insert(img1);
+    listaImagenes.insert(img2);
+    listaImagenes.insert( visu.fill(0).draw_graph(lut.get_crop(0,0,0,0,lut.width()-1,255,0,0),white,1,1,0,255,0));
+    CImgDisplay displayImagen(listaImagenes,"Logaritmica");
+
+    listaImagenes.clear();
+    img2=img1;
+    CImg<unsigned char> lut2(generar_lut_expb(cc,potencia));
+    img2=transformacion(img2,lut2);
+    listaImagenes.insert(img1);
+    listaImagenes.insert(img2);
+    listaImagenes.insert( visu.fill(0).draw_graph(lut2.get_crop(0,0,0,0,lut2.width()-1,255,0,0),white,1,1,0,255,0));
+    CImgDisplay displayImagen2(listaImagenes,"Correccion Gamma-Potencia Gamma");
+    while (!displayImagen.is_closed()) {
+        displayImagen2.wait_all();
+    }
+
+
+
+}
+
