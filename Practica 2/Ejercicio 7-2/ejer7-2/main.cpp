5e21a79cfb57e14840115426ce3afe34db6e004c
Luciano <lucianodato@gmail.com>
2015-04-04 09:39:07 -0300

    Resuelto el 7-2 de una manera sencilla usando la media de pixeles habiendo enmascarado la zona de los disipadores. Funciona incluso sin limpiar ruido. Tendria que reimplementarlo si usar la funcion media.

--- Practica 2/Ejercicio 7-2/ejer7-2/main.cpp
+++ Practica 2/Ejercicio 7-2/ejer7-2/main.cpp
@@ -3,9 +3,47 @@
 using namespace cimg_library;
 using namespace std;
 
+//Coordenadas de los puntos para aplicar la mascara (la saque con gimp)
+int m1_x = 197,m1_y= 98, m2_x = 246, m2_y = 153;
+
+const char* path1 = "../../../../images/a7v600-SE(RImpulsivo).gif";
+const char* path2 = "../../../../images/a7v600-SE.gif";
+const char* path3 = "../../../../images/a7v600-X(RImpulsivo).gif";
+const char* path4 = "../../../../images/a7v600-X.gif";
+
+
+//Establecer una forma de identificar cada imagen (Por ejemplo con el valor medio de pixel de los disipadores)
+CImg<unsigned char> img1,img2;
+const double placa1 = img1.load(path2).get_crop(m1_x,m1_y,m2_x,m2_y).mean();
+const double placa2 = img1.load(path4).get_crop(m1_x,m1_y,m2_x,m2_y).mean();
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    //Carga de la imagen
+    CImg<unsigned char> img;
+    CImg<unsigned char> img_mask;
+    img.load(path4);
+
+    //Limpiar ruido impulsivo
+
+
+    //Enmascarar la imagen en la parte de los disipadores
+    img_mask = img.get_crop(m1_x,m1_y,m2_x,m2_y);
+
+    cout<<"El promedio de pixel arrojado es "<<img_mask.mean()<<endl<<endl;
+    cout<<"El promedio de valor de pixel de a7v600-SE es "<<placa1<<endl;
+    cout<<"El promedio de valor de pixel de a7v600-X es "<<placa2<<endl<<endl;
+
+    //Comparar la imagen con los datos
+    if (img_mask.mean() >= placa1){
+        cout<<"La placa identificada es a7v600-SE"<<endl;
+    }
+    else
+    {
+        cout<<"La placa identificada es a7v600-X"<<endl;
+    }
+
+    //Retornar resultados
     return 0;
 }
 
