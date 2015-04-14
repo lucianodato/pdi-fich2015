c8bbbc73aa2a559609307229dd9cc9fa2b3317c7
Luciano <lucianodato@gmail.com>
2015-04-14 00:38:54 -0300

    Parece vivo en un universo donde el get_threshold no existe. :)

--- Practica 3/Ejercicio 5-1/ejer5-1/main.cpp
+++ Practica 3/Ejercicio 5-1/ejer5-1/main.cpp
@@ -22,49 +22,8 @@ int main()
     lista_global.display("Original - Equalizada - Hist Orig - Hist Eq");
 
     //Procesamiento por enmascaramiento de regiones
-    //Creo la mascara en base a las posiciones de los recuadros
-    mask.fill(0);
-
-    //Creo los puntos que definen cada cuadradito
-    punto a,b,c,d,e,f,g,h,i,j;
-    a.x=12;
-    a.y=12;
-
-    b.x=136;
-    b.y=136;
-
-    c.x=374;
-    c.y=12;
-
-    d.x=498;
-    d.y=136;
-
-    e.x=194;
-    e.y=194;
-
-    f.x=318;
-    f.y=318;
-
-    g.x=12;
-    g.y=376;
-
-    h.x=136;
-    h.y=500;
-
-    i.x=374;
-    i.y=376;
-
-    j.x=498;
-    j.y=500;
-
-    //Lleno la mascara con 0 en las posiciones de los cuadrados
-    llenado_puntos(mask,a,b,true);//Cuadradito 1
-    llenado_puntos(mask,c,d,true);//Cuadradito 2
-    llenado_puntos(mask,e,f,true);//Cuadradito 3
-    llenado_puntos(mask,g,h,true);//Cuadradito 4
-    llenado_puntos(mask,i,j,true);//Cuadradito 5
-
-    //mask.display();
+    //Creo la mascara en base a aislar los cuadraditos con threshold
+    mask = negativo(img.get_threshold(100));
 
     //Ecualizo solo en los cuadrados
     eq = multiplicacion(img,mask).get_equalize(256,0,255);
