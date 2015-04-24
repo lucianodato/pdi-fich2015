444c2181957d99e36b2a48e73941e4618842b45f
Luciano <lucianodato@gmail.com>
2015-04-24 18:32:09 -0300

    Ejer 3 no anda

--- Practica 4/Ejercicio 3/ejer3/main.cpp
+++ Practica 4/Ejercicio 3/ejer3/main.cpp
@@ -1,7 +1,37 @@
 #include <funciones.h>
+float thresh = 20.0;
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    const char* path = "../../../../images/rio.jpg";
+    CImg<float> img,hist;
+    img.load(path);
+
+
+    //Muestro el histograma
+    hist = img.get_histogram(256,0,255);
+    //hist.display_graph();
+
+    //Creo una imagen resutado con los canales rgb iguales a la imagen original
+    CImg<float> result(img.width(),img.height(),img.depth(),3);
+    result.channel(0) = img.get_channel(0);
+    result.channel(1) = img.get_channel(0);
+    result.channel(2) = img.get_channel(0);
+
+    result.get_channel(3).display();
+
+    //Recorro original la imagen y en funcion de la intesidad coloreo la imagen resultado
+    cimg_forXYZ(img,i,j,k){
+        if(img(i,j) <= thresh){
+            //Amarillo es 255,255,0 en los canales rgb
+            result(i,j,k,0) = 255.0;
+            result(i,j,k,1) = 255.0;
+            result(i,j,k,2) = 0.0;
+        }
+    }
+
+    result.display();
+
     return 0;
 }
 
--- funciones/funciones.h
+++ funciones/funciones.h
@@ -42,7 +42,12 @@ vector<punto> ordenarCoordenadas(vector<punto> puntos){
 
 //Colores auxiliares
 
-const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 }, white[] = { 255,255,255 }, black[] = { 0,0,0 };
+const unsigned char red[] = { 255,0,0 },
+                    green[] = { 0,255,0 },
+                    blue[] = { 0,0,255 },
+                    white[] = { 255,255,255 },
+                    black[] = { 0,0,0 },
+                    yellow[] = { 255,255,0 };
 
 
 
