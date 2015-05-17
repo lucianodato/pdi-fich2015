7e6cb0a96a00f56f1ba362fe6d1fa20e66ab380a
Luciano <lucianodato@gmail.com>
2015-05-17 12:14:20 -0300

    Misc

--- Practica 7/Ejercicio 2-1/ejer2-1/main.cpp
+++ Practica 7/Ejercicio 2-1/ejer2-1/main.cpp
@@ -31,13 +31,32 @@ int main()
 
     //Transformada inversa de Hough
 
-    CImg<double> thi_0 = hough(th_0,true);
-    CImg<double> thi_1 = hough(th_1,true);
-    CImg<double> thi_2 = hough(th_2,true);
-    CImg<double> thi_3 = hough(th_3,true);
-    CImg<double> thi_4 = hough(th_4,true);
-    CImg<double> thi_5 = hough(th_5,true);
-    CImg<double> thi_6 = hough(th_6,true);
+    //Con la imagenes originales
+    CImg<double> thi_0 = hough(hough0,true);
+    CImg<double> thi_1 = hough(hough1,true);
+    CImg<double> thi_2 = hough(hough2,true);
+    CImg<double> thi_3 = hough(hough3,true);
+    CImg<double> thi_4 = hough(hough4,true);
+    CImg<double> thi_5 = hough(hough5,true);
+    CImg<double> thi_6 = hough(hough6,true);
+
+    thi_0.display();
+    thi_1.display();
+    thi_2.display();
+    thi_3.display();
+    thi_4.display();
+    thi_5.display();
+    thi_6.display();
+
+    //Con la transformada de las imagenes
+
+    thi_0 = hough(th_0,true);
+    thi_1 = hough(th_1,true);
+    thi_2 = hough(th_2,true);
+    thi_3 = hough(th_3,true);
+    thi_4 = hough(th_4,true);
+    thi_5 = hough(th_5,true);
+    thi_6 = hough(th_6,true);
 
     thi_0.display();
     thi_1.display();
