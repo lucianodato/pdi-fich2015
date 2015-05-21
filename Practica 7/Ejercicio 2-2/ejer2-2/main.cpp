0825a4c51774dd2122bff28715bf4bcfdc59f49b
Luciano <lucianodato@gmail.com>
2015-05-21 09:41:17 -0300

    misc

--- Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
+++ Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
@@ -10,7 +10,7 @@ int main()
     //img.load("../../../../images/cameraman.tif");
     img.load("../../../../images/fourier/f8.jpg");
     //img.load("../../../../images/fourier/f3.jpg");
-    img=img.RGBtoHSI().get_channel(2);
+    //img=img.RGBtoHSI().get_channel(2);
     houghh=hough(img,false);
     houghh.normalize(0,255);
     lista.push_back(img);lista.push_back(houghh);
@@ -28,7 +28,7 @@ int main()
     //img.load("../../../../images/cameraman.tif");
     img.load("../../../../images/fourier/f8.jpg");
     //img.load("../../../../images/fourier/f3.jpg");
-    img=img.RGBtoHSI().get_channel(2);
+    //img=img.RGBtoHSI().get_channel(2);
     sobel=Sobel(img,0);
     houghh=hough(sobel,false);
     houghh.normalize(0,255);
