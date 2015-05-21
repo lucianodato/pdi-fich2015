dcd5f96d2ee7627715e41240439d503116ab53ef
Luciano <lucianodato@gmail.com>
2015-05-21 14:40:18 -0300

    Arreglos

--- Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
+++ Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
@@ -3,47 +3,6 @@ double angulo=0, rho=0.0;
 int var_rho=sqrt(2),var_angulo=90;
 int main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    CImg<double> img, sobel,houghh;
-    CImgList<double> lista;
-    //hough(CImg<double> img, double angulo,int nLineasX=0,int nLineasY=0,bool inverse=false, bool activeAngle=false) {
-
-
-    img.load("../../../../images/cameraman.tif");
-    //img.load("../../../../images/fourier/f8.jpg");
-    //img.load("../../../../images/fourier/f3.jpg");
-    //img=img.RGBtoHSI().get_channel(2);
-    houghh=hough(img,false);
-    houghh.normalize(0,255);
-    lista.push_back(img);lista.push_back(houghh);
-    houghh.threshold(255);
-    lista.push_back(houghh);
-    houghh=hough(houghh,true);
-    lista.push_back(houghh);
-    lista.display("img -- houghh   ----- hough umbral   ------ hough inversaa");
-
-
-    lista.clear();
-
-
-
-    img.load("../../../../images/cameraman.tif");
-    //img.load("../../../../images/fourier/f8.jpg");
-    //img.load("../../../../images/fourier/f3.jpg");
-    //img=img.RGBtoHSI().get_channel(2);
-    sobel=Sobel(img,0);
-    houghh=hough(sobel,false);
-    houghh.normalize(0,255);
-    lista.push_back(img);lista.push_back(houghh);
-    houghh.threshold(255);
-    lista.push_back(houghh);
-    houghh=hough(houghh,true);
-    lista.push_back(houghh);
-    lista.display("img -- sobel --- houghh  sobel   -----sobel hough umbral   ------ sobel hough inversaa");
-=======
-=======
->>>>>>> fa5707e9a01065279bf312ec0c5f30621c2526e6
     CImg<double> img, sobel,th,th_i,split,split_th_i;
         CImgList<double> lista;
         img.load("../../../../images/cameraman.tif");
@@ -67,10 +26,7 @@ int main()
 
 
         lista.display("img-img filtrada(con filtro de bordes)-- th--split hough --hough umbral --hough inversaa -- inversa de la recortada");
-<<<<<<< HEAD
->>>>>>> solo un detalle:no me segmenta en color ejer3, ver funcion ComposeHSI, ejercicio 2.2: si empiezo de cero ?
-=======
->>>>>>> fa5707e9a01065279bf312ec0c5f30621c2526e6
+        //solo un detalle:no me segmenta en color ejer3, ver funcion ComposeHSI, ejercicio 2.2: si empiezo de cero ?
 
     return 0;
 }
