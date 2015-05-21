08419f8bb12eec65d2e80204c12c1e7462765f54
Luciano <lucianodato@gmail.com>
2015-05-21 08:33:48 -0300

    Arreglo

--- Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
+++ Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
@@ -2,45 +2,42 @@
 
 int main()
 {
-<<<<<<< HEAD
-=======
     CImg<double> img, sobel,houghh;
-        CImgList<double> lista;
-  //hough(CImg<double> img, double angulo,int nLineasX=0,int nLineasY=0,bool inverse=false, bool activeAngle=false) {
+    CImgList<double> lista;
+    //hough(CImg<double> img, double angulo,int nLineasX=0,int nLineasY=0,bool inverse=false, bool activeAngle=false) {
 
 
-        //img.load("../../../../images/cameraman.tif");
-        img.load("../../../../images/fourier/f8.jpg");
-        //img.load("../../../../images/fourier/f3.jpg");
-        img=img.RGBtoHSI().get_channel(2);
-        houghh=hough(img,false);
-        houghh.normalize(0,255);
-        lista.push_back(img);lista.push_back(houghh);
-        houghh.threshold(200);
-        lista.push_back(houghh);
-        houghh=hough(houghh,true);
-        lista.push_back(houghh);
-        lista.display("img -- houghh   ----- hough umbral   ------ hough inversaa");
+    //img.load("../../../../images/cameraman.tif");
+    img.load("../../../../images/fourier/f8.jpg");
+    //img.load("../../../../images/fourier/f3.jpg");
+    img=img.RGBtoHSI().get_channel(2);
+    houghh=hough(img,false);
+    houghh.normalize(0,255);
+    lista.push_back(img);lista.push_back(houghh);
+    houghh.threshold(200);
+    lista.push_back(houghh);
+    houghh=hough(houghh,true);
+    lista.push_back(houghh);
+    lista.display("img -- houghh   ----- hough umbral   ------ hough inversaa");
 
 
-lista.clear();
+    lista.clear();
 
 
 
-        //img.load("../../../../images/cameraman.tif");
-        img.load("../../../../images/fourier/f8.jpg");
-        //img.load("../../../../images/fourier/f3.jpg");
-        img=img.RGBtoHSI().get_channel(2);
-        sobel=Sobel(img,0);
-        houghh=hough(sobel,false);
-        houghh.normalize(0,255);
-        lista.push_back(img);lista.push_back(houghh);
-        houghh.threshold(200);
-        lista.push_back(houghh);
-        houghh=hough(houghh,true);
-        lista.push_back(houghh);
-        lista.display("img -- sobel --- houghh  sobel   -----sobel hough umbral   ------ sobel hough inversaa");
->>>>>>> a94ae01b4f7ec91a0dc77488b8f0a57206e715e7
+    //img.load("../../../../images/cameraman.tif");
+    img.load("../../../../images/fourier/f8.jpg");
+    //img.load("../../../../images/fourier/f3.jpg");
+    img=img.RGBtoHSI().get_channel(2);
+    sobel=Sobel(img,0);
+    houghh=hough(sobel,false);
+    houghh.normalize(0,255);
+    lista.push_back(img);lista.push_back(houghh);
+    houghh.threshold(200);
+    lista.push_back(houghh);
+    houghh=hough(houghh,true);
+    lista.push_back(houghh);
+    lista.display("img -- sobel --- houghh  sobel   -----sobel hough umbral   ------ sobel hough inversaa");
 
     return 0;
 }
