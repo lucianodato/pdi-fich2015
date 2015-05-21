a79ea8b75709f4ee346f2ecf1f0e1ee74e2631cf
Luciano <lucianodato@gmail.com>
2015-05-21 14:36:53 -0300

    Lo mio

--- Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
+++ Practica 7/Ejercicio 2-2/ejer2-2/main.cpp
@@ -7,14 +7,14 @@ int main()
     //hough(CImg<double> img, double angulo,int nLineasX=0,int nLineasY=0,bool inverse=false, bool activeAngle=false) {
 
 
-    //img.load("../../../../images/cameraman.tif");
-    img.load("../../../../images/fourier/f8.jpg");
+    img.load("../../../../images/cameraman.tif");
+    //img.load("../../../../images/fourier/f8.jpg");
     //img.load("../../../../images/fourier/f3.jpg");
     //img=img.RGBtoHSI().get_channel(2);
     houghh=hough(img,false);
     houghh.normalize(0,255);
     lista.push_back(img);lista.push_back(houghh);
-    houghh.threshold(200);
+    houghh.threshold(255);
     lista.push_back(houghh);
     houghh=hough(houghh,true);
     lista.push_back(houghh);
@@ -25,15 +25,15 @@ int main()
 
 
 
-    //img.load("../../../../images/cameraman.tif");
-    img.load("../../../../images/fourier/f8.jpg");
+    img.load("../../../../images/cameraman.tif");
+    //img.load("../../../../images/fourier/f8.jpg");
     //img.load("../../../../images/fourier/f3.jpg");
     //img=img.RGBtoHSI().get_channel(2);
     sobel=Sobel(img,0);
     houghh=hough(sobel,false);
     houghh.normalize(0,255);
     lista.push_back(img);lista.push_back(houghh);
-    houghh.threshold(200);
+    houghh.threshold(255);
     lista.push_back(houghh);
     houghh=hough(houghh,true);
     lista.push_back(houghh);
