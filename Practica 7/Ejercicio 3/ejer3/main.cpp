61cfc2192f2bc3eb5a04e816cf6b5e3b872ad16c
cristian <cristiaan3003@gmail.com>
2015-05-25 20:52:25 -0300

    ejercicio 2 guia 7 - arreglado

--- Practica 7/Ejercicio 3/ejer3/main.cpp
+++ Practica 7/Ejercicio 3/ejer3/main.cpp
@@ -5,7 +5,7 @@
 ///sigma	Amplitude of the random additive noise. If sigma<0, it stands for a percentage of the global value range.
 ///noise_type Type of additive noise (can be 0=gaussian, 1=uniform, 2=Salt and Pepper, 3=Poisson or 4=Rician).
 ///
-int delta=150; //delta hasta el cual recortar.
+int delta=200;
 int etiqueta=256;
 int max_segm=5;
 int main()
@@ -20,52 +20,33 @@ int main()
     v1.assign(img);
     v1.set_title("click");
     v2.assign(img);//    CImgDisplay v1,v2,v3;
-    //    v1.assign(img);
-    //    v1.set_title("click");
-    //    v2.assign(img);
-    //    v2.set_title("original");
-    //    v3.assign(img);
-    //    v3.set_title("nueva");
+        v1.assign(img);
+        v1.set_title("click");
+        v2.assign(img);
+        v2.set_title("original");
+        v3.assign(img);
+        v3.set_title("nueva");
 
-    //    while(!v1.is_closed() && !v2.is_closed() && !v3.is_closed()  ){
-    //        v1.wait();
-    //        if(v1.button()==1){
-    //            //MODO DE SELECCION MANUAL DE LA SEMILLA
-    //            mx=v1.mouse_x();
-    //            my=v1.mouse_y();
-    //            imgnueva= region_growing(img,mx,my,delta,etiqueta);
+        while(!v1.is_closed() && !v2.is_closed() && !v3.is_closed()  ){
+            v1.wait();
+            if(v1.button()==1){
+                //MODO DE SELECCION MANUAL DE LA SEMILLA
+                mx=v1.mouse_x();
+                my=v1.mouse_y();
+                imgnueva= region_growing(img,mx,my,delta,etiqueta);
 
-    //            v3.render(imgnueva);
-    //            v3.paint();
-    //        }
-    v2.set_title("original");
-    v3.assign(img);
-    v3.set_title("nueva");
-    imgnueva= region_growing(img,189,239,delta,etiqueta);
-    (img,imgnueva).display();
-//    while(!v1.is_closed() && !v2.is_closed() && !v3.is_closed()  ){
-//        v1.wait();
-//        if(v1.button()==1){
-//            //MODO DE SELECCION MANUAL DE LA SEMILLA
-//            mx=v1.mouse_x();
-//            my=v1.mouse_y();
-//            imgnueva= region_growing(img,mx,my,delta,etiqueta);
+                v3.render(imgnueva);
+                v3.paint();
+            }
+        }
 
 
-//            v3.render(imgnueva);
-//            v3.paint();
-//        }}
-//        if (v1.is_key()){
 
             //MODO SELECCION AUTOMATICA DE SEMILLA
-           // imgnueva = autom_seg_region_growed(img, delta, etiqueta, max_segm);
-            //v3.render(imgnueva);
-            //v3.paint();
-            //(img,imgnueva,imgnueva.get_RGBtoHSI().get_channel(2)).display("Resultado Final");
+            imgnueva = autom_seg_region_growed(img, delta, etiqueta, max_segm);
 
-        //}
+            (img,imgnueva,imgnueva.get_RGBtoHSI().get_channel(2)).display("Resultado Final");
 
-    //}
 
     return 0;
 }
