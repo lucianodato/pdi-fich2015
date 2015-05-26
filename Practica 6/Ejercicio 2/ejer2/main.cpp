8f46addd89bdfa5a9232e9ffa64939d65bedb6b0
loquinho33 <manzorenato@gmail.com>
2015-05-26 15:48:09 -0300

    update para pull

--- Practica 6/Ejercicio 2/ejer2/main.cpp
+++ Practica 6/Ejercicio 2/ejer2/main.cpp
@@ -2,16 +2,20 @@
 
 int main()
 {
-    CImg<float> img,img_noisy,img_denoised,img_denoised2;
-    img.load("../../../../images/sangre.jpg");
+    CImg<float> img_original,img_noisy,img_denoised,img_denoised2;
+    img_original.load("../../../../images/sangre.jpg");
+    CImg<float> img(img_original.width(),img_original.height(),1,1);
 
-    img_noisy = img.get_noise(10,0).get_noise(10,2);//0=gaussian, 1=uniform, 2=Salt and Pepper, 3=Poisson or 4=Rician
-    (img,img_noisy).display();
+    img = img_original.RGBtoHSI().get_channel(2).get_normalize(0,255); //Img en escala de grisis. Canal Intensidad (2)
 
-    img_denoised = denoiseRGB(img_noisy,3,1);//Media Geometrica
-    img_denoised2 = denoiseRGB(img_noisy,3,2,-1);//Media Armonica
-    (img_denoised,img_denoised2).display();
+    img_noisy = img.get_noise(5,0).get_noise(5,1);//0=gaussian, 1=uniform, 2=Salt and Pepper, 3=Poisson or 4=Rician
+    (img,img_noisy).display("Original - Noissy");
 
+    //img_denoised = denoiseRGB(img_noisy,3,1);//Media Geometrica
+    //img_denoised2 = denoiseRGB(img_noisy,3,2,-1);//Media Armonica
+    img_denoised = denoise(img_noisy,3,1);
+    img_denoised2 = denoise(img_noisy,3,2,-1);
+    (img,img_denoised,img_denoised2).display("Original - Media Geometrica - Media Armonica (-1)");
 
     cout<<"Error cuadratico medio con la Ruidosa: "<<img.MSE(img_noisy)<<endl;
     cout<<"Error cuadratico medio con Media Geometrica: "<<img.MSE(img_denoised)<<endl;
--- Practica 7/Ejercicio 5-2/ejer5-2/main.cpp
+++ Practica 7/Ejercicio 5-2/ejer5-2/main.cpp
@@ -2,7 +2,30 @@
 
 int main()
 {
+    CImg <float> original,img,img_aux,img_res1,img_res2;
+    const char* path = "../../../../images/iguazu.jpg";
+    original.load(path);
+    original = original.RGBtoHSI().get_channel(2).get_normalize(0,255);
+
+    path = "../../../../images/iguazu_ruidogris.jpg";
+    img.load(path);
+
+    //Media Alfa Recortada d = la cantidad de pixeles de la ventana que puede recortar
+    img_res1 = denoise(img,3,5,0,5);
+    //Mediana y dsp punto medio. Bueno para mezcla de ruido impulsivo y gaussiano
+    img_aux = denoise(img,3,3);//Mediana
+    img_res2 = denoise(img_aux,3,4);//Media Punto Medio
+
+    //1 paso - Restauracion. Imagen con ruido gaussiano o impulsivo
+    original.display("Original");
+    (img,img_res1,img_res2).display("Ruidosa - Restaurada(AlfaRecort) - Restaurada(MedianayPunMedio)");
+
+    //Paso 2 - Enfasis de alta frecuencia
+
+    //Paso 3 - Segmentacion (Sobel) y transformada de hough
+
+    //Paso 4 -
+
 
     return 0;
 }
-
