d5f19c01cecd936c2b36d57615325bbe04f2a276
Luciano <lucianodato@gmail.com>
2015-05-30 08:37:12 -0300

    Ejer 8 andando. El secreto para un buen esqueleto es una buena mascara. Mientras mas limpia este la mascara mejor.

--- Practica 8/Ejercicio 8/ejer8/main.cpp
+++ Practica 8/Ejercicio 8/ejer8/main.cpp
@@ -1,24 +1,35 @@
 #include "funciones.h"
 
+#define UMBRAL 243
+
 int main()
 {
     //imagen
     CImg<float> original;
     original.load("../../../../images/Morfologia/Cuerpos.jpg");
+    CImg<bool> B(8,8),B2(3,3);
+    B.fill(1);
+    B2.fill(1);
 
     //Calculo los esqueletos con Hamilton-Jacobi
     const int median = 1;
     const bool invert = true;
-    const float thresh = -0.5f;
+    const float thresh = -0.3f;
     const bool curve = true;
     const bool correction = true;
     const float dlt1 = 2;
     const float dlt2 = 1.0f;
 
-    //Tengo que usar la imagen original pero guardar una version binaria
-    CImg<unsigned int> mascara = original.get_norm().quantize(2).normalize(0.0f,1.0f);
+    //Tengo que usar la imagen original pero guardar una version binaria (proceso conforme a esi)
+    //CImg<unsigned int> mascara = original.get_norm().quantize(2).normalize(0.0f,1.0f);
+    CImg<unsigned int> mascara = original.get_RGBtoHSI().channel(2).normalize(0,255).threshold(UMBRAL).get_normalize(0,1);
     if (median) mascara.blur_median(median);
     if (invert) (mascara-=1)*=-1;
+
+    //Segmento para que la mascara sea mejor
+    mascara = apertura(mascara,B2);
+    mascara = relleno_automatico(mascara,B);
+
 
     //Calculo el mapa de distancia
     CImg<float> distance = mascara.get_distance(0);
