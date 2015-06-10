50ec0890b13f44a41cb4b5aa258b7fd032f37a6e
Luciano <lucianodato@gmail.com>
2015-06-09 22:17:09 -0300

    Listo anda para todos los casos. No es tan trivial al final. Quizas en vez de colorslicing hubiese sido mejor usar crecimiento de regiones con condicion de color.

--- Pruebas Parcial/cervezas-rena/cerveza/main.cpp
+++ Pruebas Parcial/cervezas-rena/cerveza/main.cpp
@@ -1,7 +1,9 @@
 #include "funciones.h"
 
 #define UMBRAL 240
-#define INTENSIDAD_PROMEDIO 128.0
+#define INTENSIDAD_PROMEDIO 160.0
+#define RADIO_SLICE 100
+#define CANT_EROSION 15
 
 
 //Este metodo identifica dos tipos rubia o negra. True si es rubia, false es negra o cualquier otra
@@ -22,12 +24,10 @@ int main()
 {
     //imagen
     CImg<float> original,byn,procesada,copia,final;
-    original.load("../../../../Parcial de Practica/Cervezas/training/02.jpg");
+    original.load("../../../../Parcial de Practica/Cervezas/training/13.jpg");
     procesada = original;
     copia = original;
-    CImg<bool> B(8,8),B2(3,3),mascara;
-    B.fill(1);
-    B2.fill(1);
+    CImg<bool> mascara;
 
     //a)Encontrar la mascara del objeto.
     //1- mascara difusa, 2- escala de grises, 3 - binarizo con threadhold alto y obtengo el negativo (el fondo es blanco)
@@ -63,40 +63,32 @@ int main()
     color(0,0,0,2)=final.get_crop(cent_x-1,cent_y-1,cent_x+1,cent_y+1).channel(2).mean();
 
     //Genero la mascara
-    CImg<float> cerveza = ColorMaskHSI(final,color,10).RGBtoHSI().channel(2).normalize(0,255).threshold(10);
+    mascara = ColorMaskRGB(final.get_blur(9),color,RADIO_SLICE).RGBtoHSI().channel(2).normalize(0,1).threshold(0.01);
 
     //Elimino los pequeños pixeles y reconstruyo a partir de la que tiene el borde
-    cerveza = apertura(cerveza,mask(3));
-    cerveza = reconstruccion_dilatacion(cerveza,nerode(cerveza,mask(3),15));
-    cerveza = relleno_automatico(cerveza);
+    mascara = apertura(mascara,mask(3));
+    mascara = reconstruccion_dilatacion(mascara,nerode(mascara,mask(3),CANT_EROSION));
+    mascara = relleno_automatico(mascara);
 
     //Cuento el alto de la cerveza en si
     int alto_cerveza=0;
+    int alto_espuma=0;
+    //Cuenta espuma
     cimg_forY(final,j){
-        if(cerveza(final.width()/2,j)==1){
-            alto_cerveza++;
-        }
-    }
-
-    //Calculo el alto de la espuma haciendo la mascara necesario
-    CImg<bool> espuma = (Sobel(final,0) + Sobel(final,1)).get_RGBtoHSI().channel(2).normalize(0,255).threshold(10);
-
-    //Elimino los pequeños pixeles y reconstruyo a partir de la que tiene el borde
-    espuma = apertura(espuma,mask(3));
-    espuma = reconstruccion_dilatacion(espuma,nerode(espuma,mask(3),5));
-    espuma = relleno_automatico(espuma);
-
-    //La parte de la cerveza en blanco
-
-    //Recorro la mascara desde arriba para abajo y almaceno la cantidad de pixeles
-    int alto_espuma = 0;
-    cimg_forY(final,j){
-        if(espuma(final.width()/2,j)==0){
+        if(mascara(final.width()/2,j)==0){
             alto_espuma++;
         }else{
             break;
         }
     }
+    //Cuenta cerveza
+    cimg_forY(final,j){
+        if(mascara(final.width()/2,j)==1){
+            alto_cerveza++;
+        }
+    }
+
+    (original,final,color,mascara).display();
 
     //Calculo el porcentaje de espuma
     cout<<"El porcentaje de espuma es: "<<alto_espuma*100/(alto_cerveza+alto_espuma)<<"%"<<endl;
