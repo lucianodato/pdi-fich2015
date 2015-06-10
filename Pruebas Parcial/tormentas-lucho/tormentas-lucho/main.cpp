8962a1285ec9fbe9aeb84f7b5046a86a84002fd9
Luciano <lucianodato@gmail.com>
2015-06-10 09:08:08 -0300

    Comenzando tormenta

---
+++ Pruebas Parcial/tormentas-lucho/tormentas-lucho/main.cpp
@@ -0,0 +1,16 @@
+#include "funciones.h"
+
+int main()
+{
+    //imagen
+    CImg<float> original;
+    original.load("../../../../Parcial de Practica/tormentas/11.jpg");
+
+    //Creo mascara para aislar el circulo central
+
+
+
+
+    return 0;
+}
+
